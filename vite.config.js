e180019c6e90725bbed2878917618c18856b7ad2
Bernardo Estrada <estradaf.bernardo@gmail.com>
2021-09-27 14:54:23 -0500

    add build config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,20 @@
 import vue from '@vitejs/plugin-vue'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
-export default {
-  process.env.NODE_ENV === 'production'
-    ? '/vue-crash-course/'
-    : '/'
-  plugins: [
-    vue()
-  ],
+export default () =>{
+  return defineConfig({
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, 'index.html'),
+          // intro: resolve(__dirname, '1-intro/index.html'),
+          formValidation: resolve(__dirname, '2-form-validation/index.html'),
+        }
+      }
+    },
+    plugins: [
+      vue()
+    ],
+  })
 }
