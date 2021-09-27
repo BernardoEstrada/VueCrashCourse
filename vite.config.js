1a819ddd33392f76bfae9b10d41f4c36434e7e99
Bernardo Estrada <estradaf.bernardo@gmail.com>
2021-09-27 15:19:18 -0500

    Fix GH action

--- vite.config.js
+++ vite.config.js
@@ -8,9 +8,15 @@ export default () =>{
       rollupOptions: {
         input: {
           main: resolve(__dirname, 'index.html'),
-          // intro: resolve(__dirname, '1-intro/index.html'),
+          intro: resolve(__dirname, '1-intro/index.html'),
           formValidation: resolve(__dirname, '2-form-validation/index.html'),
-        }
+          pokemon: resolve(__dirname, '3-pokemon/index.html'),
+          composition: resolve(__dirname, '4-composition/index.html'),
+          microblog: resolve(__dirname, '5-microblog/index.html'),
+          router: resolve(__dirname, '6-router/index.html'),
+          routerOptions: resolve(__dirname, '6-router-options/index.html'),
+        },
+        external: true,
       }
     },
     plugins: [
